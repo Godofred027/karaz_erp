2ded69c86ccfd2f3d695151fec23eedd8dc30af3
Andres Paredes <godofredo.24.p@gmail.com>
2022-12-26 18:14:07 -0500

    routes dinamics

--- src/controllers/documents.controller.js
+++ src/controllers/documents.controller.js
@@ -17,16 +17,3 @@ export const uploadDocument = upload.single("document");
 export const getDocuments = async (req, res) => {
   res.send("Document Uploaded");
 };
-
-export const showDocuments = async (req, res) => {
-  res.json([
-    {
-      name: "Recibo Claro",
-      url: "http://localhost:5000/public/documentsTest/1671846833215-recibo.pdf",
-    },
-    {
-      name: "Foto Random",
-      url: "http://localhost:5000/public/documentsTest/1.jpg",
-    },
-  ]);
-};
--- src/routes/documents.routes.js
+++ src/routes/documents.routes.js
@@ -3,12 +3,9 @@ import { Router } from "express";
 import {
   uploadDocument,
   getDocuments,
-  showDocuments,
 } from "../controllers/documents.controller.js";
 
 const router = Router();
-
-router.get("/", showDocuments);
 
 router.post("/", uploadDocument, getDocuments);
 
