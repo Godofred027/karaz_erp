2a8aea71e733a17b0a5c16884e88d01b47793417
Andres Paredes <godofredo.24.p@gmail.com>
2022-12-26 22:03:16 -0500

    update

--- src/models/User.js
+++ src/models/User.js
@@ -1,6 +1,6 @@
 import { DataTypes } from "sequelize";
 import { sequelize } from "../database/database.js";
-import { Role } from "./role.js";
+import { Role } from "./Role.js";
 
 export const User = sequelize.define(
   "user",
